f1b4a7fd6a5931414e7042aece1d711ed2c609c4
fatosdagdevir <fatma.dagdevir@gmail.com>
2025-07-07 16:30:59 +0200

    Initial Commit

---
+++ PhotoGrid/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PhotoGrid
+//
+//  Created by Fatma Dagdevir on 07.07.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PhotoGridTests/PhotoGridTests.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoGridTests.swift
+//  PhotoGridTests
+//
+//  Created by Fatma Dagdevir on 07.07.25.
+//
+
+import Testing
+@testable import PhotoGrid
+
+struct PhotoGridTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
